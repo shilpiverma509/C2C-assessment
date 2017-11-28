b3d75090d61ffd7d96ca24fdb102367c068f1bf7
Shilpi Verma <shilpiverma509@gmail.com>
2017-11-28 16:13:08 -0600

    C2c assessment

---
+++ index.js
@@ -0,0 +1,8 @@
+function  clickMe(ele) {
+    if (ele.innerHTML == 'facebook') {
+        ele.innerHTML = 'Shilpi';
+    } else {
+        ele.innerHTML = 'facebook';
+    }   
+    return false;
+}
